db16fc2cad698d142d85c745239c38f97c402c42
Krasimir <krasimir.st.tsonev@gmail.com>
2024-07-28 13:54:26 +0300

    Progress on the file explorer

--- client/src/js/FileExplorer.tsx
+++ client/src/js/FileExplorer.tsx
@@ -1,4 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import Editor from './Editor';
+
+type Item = {
+  path: string,
+  name: string,
+  type: 'folder' | 'file',
+  children: Item[]
+}
 
 export default function FileExplorer() {
   const [ error, setError ] = useState(null);
@@ -19,10 +27,43 @@ export default function FileExplorer() {
     })();    
   }, []);
 
+  function renderDir(item: Item, level = 0) {
+    return (
+      <div key={item.path}>
+        <button className='flex as-link' style={calculateIndent(level)}>
+          <img src='./imgs/folder.svg' width={22} className='mr02' />
+          <span>{item.name}</span>
+        </button>
+        {item.children.map(child => (
+          child.type === 'folder' ? renderDir(child, level + 1) : renderFile(child, level + 1)
+        ))}
+      </div>
+    )
+  }
+  function renderFile(item: Item, level = 0) {
+    return (
+      <button key={item.path} className='flex as-link' style={calculateIndent(level)}>
+        <img src='./imgs/file-text.svg' width={22} className='mr02' />
+        <span>{item.name}</span>
+      </button>
+    )
+  }
+
   return (
-    <div className="abs curtain fill" style={{ top: 0, left: 0 }}>
+    <div className="abs curtain fill fz12" style={{ top: 0, left: 0 }} id="file-explorer">
+      <button className="icon abs" style={{ top: '1em', right: '1em' }} onClick={() => {
+        Editor.instance.toggleFileExporer();
+      }}>
+        <img src='./imgs/x-circle.svg' alt='close'/>
+      </button>
       {error && <div className='p1'>{error.toString()}</div>}
-      
+      <div className='p1'>
+        {files.map(dir => renderDir(dir))}
+      </div>
     </div>
   ) 
+}
+
+function calculateIndent(level: number) {
+  return { marginLeft: `${level * 0.8}em` };
 }
