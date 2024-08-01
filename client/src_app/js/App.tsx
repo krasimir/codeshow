460e88cdded866e89a0c8bc0d9738bbeb96cbf19
Krasimir <krasimir.st.tsonev@gmail.com>
2024-08-01 12:40:52 +0300

    Working on the script

--- client/src_app/js/App.tsx
+++ client/src_app/js/App.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [ answer, setAnswer ] = useState(null);
+
   return (
     <div>
-      Hello world
+      <div>
+        <div>First name:</div>
+        <input type="text" />        
+      </div>
+      <div class="mt-6 mb-6">
+        <div>Are you a web developer?</div>
+        <input type="radio" checked={answer === 'yes'} onClick={() => setAnswer('yes')}/> 
+        <span onClick={() => setAnswer('yes')} class="mr-3">Yes</span>
+        <input type="radio" checked={answer === 'no'} onClick={() => setAnswer('no')}/> 
+        <span onClick={() => setAnswer('no')}>No</span>
+      </div>
+      <div class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-center" style={{ cursor: "pointer" }}>Register</div>
     </div>
   )
 }
