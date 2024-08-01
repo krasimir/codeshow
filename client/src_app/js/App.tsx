d83fa34e2abb8e50e28257a4a105990a4f120830
Krasimir <krasimir.st.tsonev@gmail.com>
2024-08-01 23:06:40 +0300

    Working on the script

--- client/src_app/js/App.tsx
+++ client/src_app/js/App.tsx
@@ -1,24 +1,25 @@
 import React, { useState } from 'react';
 
 export default function App() {
-  const [ answer, setAnswer ] = useState(null);
-  function onSubmit() {
-    // ...
+  function onSubmit(e) {
+    e.preventDefault();
   }
   return (
-    <div>
-      <div>
+    <form onSubmit={onSubmit}>
+      <fieldset>
         <div>First name:</div>
         <input type="text" />        
-      </div>
-      <div className="mt-6 mb-6">
+      </fieldset>
+      <fieldset className="mt-6 mb-6">
         <div>Are you a web developer?</div>
-        <input type="radio" checked={answer === 'yes'} onClick={() => setAnswer('yes')}/> 
-        <span onClick={() => setAnswer('yes')} className="mr-3">Yes</span>
-        <input type="radio" checked={answer === 'no'} onClick={() => setAnswer('no')}/> 
-        <span onClick={() => setAnswer('no')}>No</span>
-      </div>
-      <div className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-center cursor-pointer" onClick={onSubmit}>Register</div>
-    </div>
+        <label>
+        <input type="radio" name="question"/> Yes
+        </label>
+        <label className="ml-3">
+        <input type="radio" name="question"/> No
+        </label>
+      </fieldset>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-center cursor-pointer">Register</button>
+    </form>
   )
 }
