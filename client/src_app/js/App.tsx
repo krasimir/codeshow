7db7c7a4064283a44eac3ad4edd09681752140b9
Krasimir <krasimir.st.tsonev@gmail.com>
2024-08-02 23:01:24 +0300

    Small changes

--- client/src_app/js/App.tsx
+++ client/src_app/js/App.tsx
@@ -1,21 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 export default function App() {
-  function onSubmit() {
-    // ...
-  }
   return (
-    <form>
-      <fieldset>
+    <div>
+      <div>
         <div>First name:</div>
         <input type="text" />        
-      </fieldset>
-      <fieldset className="mt-6 mb-6">
+      </div>
+      <div className="mt-6 mb-6">
         <div>Are you a web developer?</div>
         <input type="radio" /> Yes
         <input type="radio" /> No
-      </fieldset>
-      <div className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-center cursor-pointer" onClick={onSubmit}>Register</div>
-    </form>
+      </div>
+      <div className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-center cursor-pointer">Register</div>
+    </div>
   )
 }
